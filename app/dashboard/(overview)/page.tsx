b37512102979ea57f3084d44ae8d7f298455e5d2
ngoquoctoandev <ngoquoctoandev@gmail.com>
2024-01-02 04:12:25 +0700

    add auth and metadata

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,39 @@
+import { Card } from '@/app/dashboard/cards';
+import CardWrapper from '@/app/dashboard/cards';
+import RevenueChart from '@/app/dashboard/revenue-chart';
+import LatestInvoices from '@/app/dashboard/latest-invoices';
+import { inter } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import Form from '@/app/dashboard/invoices/create-form';
+import Breadcrumbs from '@/app/dashboard/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
